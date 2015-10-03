9473ccfe0e1b5d3bb34b9babcc073e36e207b9cd
Andrew Munro <andrew@sumobi.com>
2015-10-03 21:53:28 +1300

    add external icon to pricing buttons

--- functions.php
+++ functions.php
@@ -83,13 +83,29 @@ function rcp_nav_home() {
 <?php }
 
 /**
+ * Add external icon to pricing buttons
+ *
+ * @since 1.0.0
+ */
+function rcp_edd_pricing_table_purchase_button_end() {
+	?>
+	<img class="icon-external" src="<?php echo get_stylesheet_directory_uri() . '/images/svgs/combined/external.svg'; ?>" />
+<?php
+}
+add_action( 'edd_pricing_table_purchase_button_end', 'rcp_edd_pricing_table_purchase_button_end' );
+
+
+/**
  * Add a notice underneath the pricing table
  *
  * @since 1.0.0
  */
 function rcp_pricing_table_notice() {
 	?>
-	<p class="trustedd-notice">After choosing a pricing option you will be redirected to PippinsPlugins.com for payment.</p>
+	<p class="trustedd-notice aligncenter">After choosing a pricing option you will be redirected to PippinsPlugins.com for payment.</p>
+
+	<p class="trustedd-notice aligncenter">* You must renew the license after one calendar year for continued updates and support. All purchases are subject to our terms and condition of use.</p>
+
 <?php
 }
 add_action( 'edd_pricing_table_bottom', 'rcp_pricing_table_notice' );
--- includes/pricing.php
+++ includes/pricing.php
@@ -2,12 +2,12 @@
 
 /**
  * Set up the pricing table
- * 
+ *
  * For now this filters the edd pricing tables plugin which will eventually provide a UI
  */
 function rcp_edd_pricing_table_settings( $args ) {
 
-	$args = array( 
+	$args = array(
 		'columns' => 3,
 		'options' => array(
 			'1' => array(
@@ -16,6 +16,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'price'       => 42,
 				'price_option' => 0,
 				'button_link' => 'https://pippinsplugins.com/checkout/',
+				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>1 site</strong>',
 					2 => 'Updates for <strong>1 year</strong> *',
@@ -28,6 +29,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'price'       => 86,
 				'price_option' => 1,
 				'button_link' => 'https://pippinsplugins.com/checkout/',
+				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>2 to 5 sites</strong>',
 					2 => 'Updates for <strong>1 year</strong> *',
@@ -40,6 +42,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'price'       => 132,
 				'price_option' => 2,
 				'button_link' => 'https://pippinsplugins.com/checkout/',
+				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>Unlimited sites</strong>',
 					2 => 'Updates for <strong>1 year</strong> *',
@@ -48,7 +51,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 			),
 		)
 	);
-	
+
 	return $args;
 }
 add_filter( 'edd_pricing_table_settings', 'rcp_edd_pricing_table_settings' );
