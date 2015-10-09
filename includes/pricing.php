983587ec737b5dd23653c3c6797c472b07752fbd
Pippin Williamson <pippin@pippinsplugins.com>
2015-10-09 08:41:32 -0500

    Add campaign tracking links

--- includes/pricing.php
+++ includes/pricing.php
@@ -15,7 +15,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'id'          => 7460,
 				'price'       => 42,
 				'price_option' => 0,
-				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site',
+				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site&utm_source=rcp-site&utm_medium=button&utm_campaign=RCP%20Site',
 				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>1 site</strong>',
@@ -29,7 +29,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'id'          => 7460,
 				'price'       => 86,
 				'price_option' => 1,
-				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site',
+				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site&utm_source=rcp-site&utm_medium=button&utm_campaign=RCP%20Site',
 				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>2 to 5 sites</strong>',
@@ -43,7 +43,7 @@ function rcp_edd_pricing_table_settings( $args ) {
 				'id'          => 7460,
 				'price'       => 132,
 				'price_option' => 2,
-				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site',
+				'button_link' => 'https://pippinsplugins.com/checkout/?referrer=rcp_site&utm_source=rcp-site&utm_medium=button&utm_campaign=RCP%20Site',
 				'button_class' => 'external',
 				'features' => array(
 					1 => '<strong>Unlimited sites</strong>',
