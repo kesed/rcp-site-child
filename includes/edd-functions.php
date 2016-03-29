0a775d823faed8f68082e02f40ca279c44baff38
Andrew Munro <andrew@sumobi.com>
2016-03-29 14:28:08 +1300

    terms checkbox and lightbox

--- includes/edd-functions.php
+++ includes/edd-functions.php
@@ -1,6 +1,76 @@
 <?php
 
 /**
+ * Link to terms page
+ * @return [type] [description]
+ */
+function rcp_edd_terms_agreement() {
+	global $edd_options;
+
+	if ( isset( $edd_options['show_agree_to_terms'] ) ) : ?>
+
+
+	<fieldset id="edd_terms_agreement">
+		<input name="edd_agree_to_terms" class="required" type="checkbox" id="edd_agree_to_terms" value="1" />
+		<label for="edd_agree_to_terms">
+			I acknowledge and agree that I am purchasing a subscription and have read the <?php echo '<a href="#refund-policy" class="popup-content" data-effect="mfp-move-from-bottom">purchase terms and refund policy</a>'; ?>
+		</label>
+	</fieldset>
+
+	<?php // seems to only work when placed here ?>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+
+		// inline
+		$('.popup-content').magnificPopup({
+			type: 'inline',
+			fixedContentPos: true,
+			fixedBgPos: true,
+			overflowY: 'scroll',
+			closeBtnInside: true,
+			preloader: false,
+			callbacks: {
+				beforeOpen: function() {
+				this.st.mainClass = this.st.el.attr('data-effect');
+				}
+			},
+			midClick: true,
+			removalDelay: 300
+        });
+
+		});
+	</script>
+
+	<?php endif;
+}
+remove_action( 'edd_purchase_form_before_submit', 'edd_terms_agreement' );
+add_action( 'edd_purchase_form_before_submit', 'rcp_edd_terms_agreement' );
+
+
+/**
+ * Terms and conditions
+ */
+function rcp_show_refund_policy() {
+
+	if ( ! function_exists( 'edd_is_checkout' ) || ! edd_is_checkout() ) {
+		return;
+	}
+
+	$post = get_page_by_title( 'purchase terms and refund policy' );
+
+	?>
+
+	<div id="refund-policy" class="popup entry-content mfp-with-anim mfp-hide">
+		<h1><?php echo $post->post_title; ?></h1>
+		<?php echo wpautop( $post->post_content, true ); ?>
+	</div>
+
+	<?php
+}
+add_action( 'wp_footer', 'rcp_show_refund_policy' );
+
+
+/**
  * Redirect to pricing page when cart is empty.
  *
  * @return void
