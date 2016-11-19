e85be05661f2e09605b993b45436301fb44d1c4e
Andrew Munro <andrew@sumobi.com>
2016-11-19 11:37:18 -0800

    remove changelog.php

--- changelog.php
+++
@@ -1,6 +0,0 @@
-<?php require_once('../../../wp-load.php'); ?>
-
-<div id="changelog" class="popup entry-content mfp-with-anim">
-    <h1>Changelog</h1>
-    <?php echo rcp_get_changelog(); ?>
-</div>
