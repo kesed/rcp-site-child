f81c8f9ff1887f5341ed034824c793e9d7579bd4
Andrew Munro <andrew@sumobi.com>
2016-11-19 11:37:55 -0800

    update footer link and tidyup

--- includes/theme-tweaks.php
+++ includes/theme-tweaks.php
@@ -202,7 +202,8 @@ function rcp_footer_menu() {
 					<li><a href="<?php echo site_url( 'support' ); ?>">Support</a></li>
 					<li><a href="http://docs.pippinsplugins.com/collection/4-restrict-content-pro" target="_blank">Documentation</a></li>
 					<li><a href="<?php echo site_url( 'screenshots' ); ?>">Screenshots</a></li>
-                    <li><a href="<?php echo get_stylesheet_directory_uri() . '/changelog.php'; ?>" id="rcp-changelog" class="popup-content download-meta-link" data-effect="mfp-move-from-bottom">Changelog</a></li>
+                    <?php /* <li><a href="<?php echo get_stylesheet_directory_uri() . '/changelog.php'; ?>" id="rcp-changelog" class="popup-content download-meta-link" data-effect="mfp-move-from-bottom">Changelog</a></li> */ ?>
+					<li><a href="<?php echo site_url( 'changelog' ); ?>">Changelog</a></li>
 					<li><a href="<?php echo site_url( 'add-ons' ); ?>">Add-ons</a></li>
 					<li><a href="<?php echo site_url( 'about' ); ?>">About</a></li>
 					<li><a href="<?php echo site_url( 'consultants' ); ?>">Consultants</a></li>
@@ -281,87 +282,6 @@ function rcp_footer_menu() {
 <?php
 }
 add_action( 'themedd_footer_start', 'rcp_footer_menu' );
-
-/**
- * Get changelog
- */
-function rcp_get_changelog() {
-
-    // Check for transient, if none, grab remote HTML file
-	if ( false === ( $html = get_transient( 'pp_rcp_changelog' ) ) ) {
-
-        // Get remote HTML file
-		$response = wp_remote_get( 'https://restrictcontentpro.com/downloads/restrict-content-pro/changelog' );
-
-        // Check for error
-		if ( is_wp_error( $response ) ) {
-			return;
-		}
-
-        // Parse remote HTML file
-		$data = wp_remote_retrieve_body( $response );
-
-        // Check for error
-		if ( is_wp_error( $data ) ) {
-			return;
-		}
-
-        // Store remote HTML file in transient, expire after 24 hours
-		set_transient( 'pp_rcp_changelog', $data, 24 * HOUR_IN_SECONDS );
-
-	}
-
-	if ( $html ) {
-		return stripslashes_deep( $html );
-	} else {
-		return stripslashes_deep( $data );
-	}
-
-}
-
-/**
- * Clears the changelog transient when the RCP download is saved.
- */
-function rcp_delete_changelog_transient( $post_id ) {
-	if ( rcp_get_download_id() == $post_id ) {
-		delete_site_transient( 'pp_rcp_changelog' );
-	}
-}
-add_action( 'save_post_download', 'rcp_delete_changelog_transient' );
-
-/**
- * Changelog
- *
- * @since 1.0.0
- */
-function rcp_product_changelog() {
-	?>
-    <script type="text/javascript">
-         jQuery(document).ready(function($) {
-
-           $('#rcp-changelog').magnificPopup({
-             type: 'ajax',
-             fixedContentPos: true,
-             alignTop: true,
-             fixedBgPos: true,
-             overflowY: 'scroll', // as we know that popup content is tall we set scroll overflow by default to avoid jump
-             closeBtnInside: true,
-             preloader: false,
-             callbacks: {
-                 beforeOpen: function() {
-                 this.st.mainClass = this.st.el.attr('data-effect');
-                 }
-             },
-             midClick: true,
-             removalDelay: 300
-           });
-
-         });
-       </script>
-
-	<?php
-}
-add_action( 'wp_footer', 'rcp_product_changelog', 100 );
 
 /**
  * Load the subpages sidebar for subpages
