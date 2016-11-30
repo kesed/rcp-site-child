7f55db5f04ffbf8d9e9a05eeab166db19b6b6d60
Andrew Munro <andrew@sumobi.com>
2016-11-30 01:21:27 -0500

    Changelog tweaks

--- includes/changelog.php
+++ includes/changelog.php
@@ -2,7 +2,7 @@
 /**
  * Changelog
  *
- * Loads AffiliateWP's changelog via ajax, caches it, and displays it in a modal window
+ * Loads Restrict Content Pro's changelog via ajax, caches it, and displays it in a modal window
  */
 
 // Exit if accessed directly
@@ -20,6 +20,7 @@ function rcp_theme_append_changelog() {
 	}
 
 	echo rcp_get_changelog();
+
 }
 add_action( 'themedd_entry_content_end', 'rcp_theme_append_changelog' );
 
@@ -28,35 +29,21 @@ add_action( 'themedd_entry_content_end', 'rcp_theme_append_changelog' );
  */
 function rcp_get_changelog() {
 
-    // Check for transient, if none, grab remote HTML file
-	if ( false === ( $html = get_transient( 'pp_rcp_changelog' ) ) ) {
+	// Check for transient, if none, grab post meta
+	if ( false === ( $changelog = get_transient( 'pp_rcp_changelog' ) ) ) {
 
-        // Get remote HTML file
-		$response = wp_remote_get( 'https://restrictcontentpro.com/downloads/restrict-content-pro/changelog/' );
+		$changelog = get_post_meta( rcp_get_download_id(), '_edd_sl_changelog', true );
 
-        // Check for error
-		if ( is_wp_error( $response ) ) {
-			return;
-		}
-
-        // Parse remote HTML file
-		$data = wp_remote_retrieve_body( $response );
-
-        // Check for error
-		if ( is_wp_error( $data ) ) {
-			return;
-		}
-
-        // Store remote HTML file in transient, expire after 24 hours
-		set_transient( 'pp_rcp_changelog', $data, 24 * HOUR_IN_SECONDS );
+		// Store remote HTML file in transient, expire after 24 hours
+		set_transient( 'pp_rcp_changelog', $changelog, 24 * HOUR_IN_SECONDS );
 
 	}
 
-	if ( $html ) {
-		return stripslashes_deep( $html );
-	} else {
-		return stripslashes_deep( $data );
+	if ( $changelog ) {
+		return stripslashes_deep( $changelog );
 	}
+
+	return false;
 
 }
 
@@ -66,7 +53,7 @@ function rcp_get_changelog() {
 function rcp_delete_changelog_transient( $post_id ) {
 
 	if ( rcp_get_download_id() == $post_id ) {
-		delete_site_transient( 'pp_rcp_changelog' );
+		delete_transient( 'pp_rcp_changelog' );
 	}
 
 }
